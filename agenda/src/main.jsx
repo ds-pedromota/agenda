e31483644ba08c008aae0dc5dc2aefa6b33eceeb
Pedro Mota <devjpmota@gmail.com>
2026-04-30 11:34:26 -0300

    up

--- agenda/src/main.jsx
+++ agenda/src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import PainelMestre from './painel-mestre.jsx'
+import PlanoSemanal from './plano-semanal.jsx'
+
+const routes = {
+  '/': PainelMestre,
+  '/plano-semanal': PlanoSemanal,
+}
+
+const Page = routes[window.location.pathname] ?? PainelMestre
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <PainelMestre />
+    <Page />
   </StrictMode>,
 )
