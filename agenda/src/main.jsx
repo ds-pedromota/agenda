b336ed13f57ff7287e64490a1a7c50e14352f3cb
Pedro Mota <devjpmota@gmail.com>
2026-05-13 17:18:08 -0300

    Update main.jsx

--- agenda/src/main.jsx
+++ agenda/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import PainelMestre from './painel-mestre.jsx'
 import PlanoSemanal from './plano-semanal.jsx'
+import RotinaTrabalho from './Rotina_Trabalho_Pedro.jsx'
 
 const routes = {
   '/': PainelMestre,
   '/plano-semanal': PlanoSemanal,
+  '/rotina-trabalho': RotinaTrabalho,
 }
 
 const Page = routes[window.location.pathname] ?? PainelMestre
